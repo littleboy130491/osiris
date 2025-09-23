6f398b83ab850ca3e43d3faed0073a2e9e460cf2
unknown <littleboy_13@yahoo.com>
2025-09-23 10:18:50 +0700

    Hardcode API_ENDPOINT tracker.js

--- config/osiris.php
+++ config/osiris.php
@@ -5,5 +5,6 @@ return [
     'crm-api-token' => env('CRM_API_TOKEN', 'lorem-ipsum'),
     'restrict_domain'   => env('RESTRICT_DOMAIN', false),
     'allowed-domains' => ['https://imajiner.id'],
+    'crm-url' => env('OSIRIS_CRM_URL', 'https://osiris.imajiner.id'),
 
 ];
